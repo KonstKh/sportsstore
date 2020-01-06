e6f96f3c1284e0bd924c13f2c388c203bcdf77da
KonstKh <const.kharchenko@gmail.com>
2020-01-06 18:03:50 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import { SportsStoreDataStore } from './data/dataStore';
+import { ShopConnector } from './shop/ShopConnector';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={SportsStoreDataStore}>
+      <Router>
+        <Switch>
+          <Route path="/shop" component={ShopConnector} />
+          <Redirect to="/shop" />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
